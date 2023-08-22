6709805eb70ef58d1c462b74fdbdc7f54fd089a0
syedjunaid7 <99808491+syedjunaid7@users.noreply.github.com>
2023-08-23 00:00:06 +0530

    c

--- src/theme.js
+++ src/theme.js
@@ -40,13 +40,30 @@ export const theme = createTheme({
     primary: {
       main: shades.primary[500],
     },
-    secodary : {
-        main : shades.secondary[500]
+    secodary: {
+      main: shades.secondary[500],
     },
-    neutral : {
-        dark : shades.neutral[700],
-        main : shades.neutral[500],
-        light : shades.neutral[100],
-    }
+    neutral: {
+      dark: shades.neutral[700],
+      main: shades.neutral[500],
+      light: shades.neutral[100],
+    },
+  },
+  typography: {
+    fontFamily: ["Fauna One", "sans-serif"].join(","),
+    fontSize: 11,
+    h1: {
+      fontFamily: ["Cinzel", "sans-serif"].join(","),
+      fontSize: 48,
+    },h2: {
+        fontFamily: ["Cinzel", "sans-serif"].join(","),
+        fontSize: 36,
+      },h3: {
+        fontFamily: ["Cinzel", "sans-serif"].join(","),
+        fontSize: 20,
+      },h4: {
+        fontFamily: ["Cinzel", "sans-serif"].join(","),
+        fontSize: 14,
+      },
   },
 });
