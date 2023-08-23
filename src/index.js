b43a9acb2d3d24e6d8115160b71fda043dc691ec
syedjunaid7 <99808491+syedjunaid7@users.noreply.github.com>
2023-08-23 21:28:31 +0530

    c

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { ThemeProviderProps } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+import { theme } from "./theme";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProviderProps theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProviderProps>
   </React.StrictMode>
 );
-
