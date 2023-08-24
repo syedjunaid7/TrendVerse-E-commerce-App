7d54b2703ef16aacffb6bad8c0bc26820d3dec70
syedjunaid7 <99808491+syedjunaid7@users.noreply.github.com>
2023-08-24 21:13:52 +0530

    c

--- src/scenes/global/Navbar.jsx
+++ src/scenes/global/Navbar.jsx
@@ -1,17 +1,36 @@
-import React from 'react'
-import {useDispatch, useSelector} from 'react-redux';
-import {Badge, Box, IconButton} from "@mui/material";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Badge, Box, IconButton } from "@mui/material";
 import {
-    PersonOutline,
-    ShoppingBagOutLined,
-    MenuOutlined,
-    SearchOutlined
+  PersonOutline,
+  ShoppingBagOutLined,
+  MenuOutlined,
+  SearchOutlined,
 } from "@mui/icons-material";
-import { useNavigate } from 'react-router-dom';
-import {shades} from "../../theme"
+import { useNavigate } from "react-router-dom";
+import { shades } from "../../theme";
 
 export default function Navbar() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   return (
-    <div>Navbar</div>
-  )
+    <Box
+      display="flex"
+      alignItems="center"
+      width="100%"
+      height="60px"
+      backgroundColor="rgba(255, 255, 255, 0.95)"
+      color="black"
+      position="fixed"
+      top="0"
+      left="0"
+      zIndex="1"
+    >
+        <Box 
+        width="80%"
+        
+        
+        ></Box>
+    </Box>
+  );
 }
