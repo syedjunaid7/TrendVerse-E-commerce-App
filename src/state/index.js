9e489f593dc42920f16d0577239592f3e6265a3a
syedjunaid7 <99808491+syedjunaid7@users.noreply.github.com>
2023-08-29 22:41:05 +0530

    c

--- src/state/index.js
+++ src/state/index.js
@@ -5,3 +5,15 @@ const initialState = {
     cart : [],
     items : [],
 }
+export const cartSlice = createSlice({
+    name : "cart",
+    initialState, //This is the initial value of the items field when the Redux store is initialized
+    reducers : {
+        setItems : (state, action) => {
+            // state.items refers to the current value of the items field in the Redux state.
+            state.items = action.payload //It is a way to pass data from an action to the corresponding reducer, allowing to update the Redux state
+            //by setting state.items to action.payload, 
+            // updating the items field in the Redux state with the new data from the action.
+        } 
+    }
+})
