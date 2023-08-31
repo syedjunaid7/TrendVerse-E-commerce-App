106118e3f970139a14cbca172157a26dfbd6c52a
syedjunaid7 <99808491+syedjunaid7@users.noreply.github.com>
2023-08-31 18:33:30 +0530

    c

--- src/scenes/global/CartMenu.jsx
+++ src/scenes/global/CartMenu.jsx
@@ -20,19 +20,33 @@ const FlexBox = styled(Box)`
 `;
 
 const CartMenu = () => {
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-    const cart = useSelector((state) => state.cart.cart);
-    const isCartOpen = useSelector((state) => state.cart.isCartOpen);
-  
-    const totalPrice = cart.reduce((total, item) => {
-      return total + item.count * item.attributes.price;
-    }, 0);
-    return (
-      <B0x
-      display ={isCartOpen ? "block" : "none"}
-      >
-        
-      </B0x>
-    )
-}  
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart.cart);
+  const isCartOpen = useSelector((state) => state.cart.isCartOpen);
+
+  const totalPrice = cart.reduce((total, item) => {
+    return total + item.count * item.attributes.price;
+  }, 0);
+  return (
+    <B0x
+      display={isCartOpen ? "block" : "none"}
+      backGroundColor="rgba(0,0,0,0.4)"
+      z-inddex={10}
+      width="100%"
+      height="100%"
+      left="0"
+      top="0"
+      overflow="auto"
+    >
+      <Box
+        position="fixed"
+        right="0"
+        bottom="0"
+        width="max(400px, 30%)"
+        height="100%"
+        backGroundColor="white"
+      ></Box>
+    </B0x>
+  );
+};
