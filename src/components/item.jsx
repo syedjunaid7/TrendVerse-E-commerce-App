29275b81d5a757649bc8ccd2cf43eaf88fe4a748
syedjunaid7 <99808491+syedjunaid7@users.noreply.github.com>
2023-09-01 21:54:49 +0530

    c

--- src/components/item.jsx
+++ src/components/item.jsx
@@ -1,12 +1,32 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-import {IconButton, Box, Typography, useTheme, Button} from "@mui/material"
-import AddIcon from "@mui/material/Add"
-import RemoveIcon from "@mui/icons-material"
-import {shades} from "../theme"
+import { IconButton, Box, Typography, useTheme, Button } from "@mui/material";
+import AddIcon from "@mui/material/Add";
+import RemoveIcon from "@mui/icons-material";
+import { shades } from "../theme";
 import { addToCart } from "../state";
 import { useNavigate } from "react-router-dom";
 
-const Item = ({item,width}) => {
-    
-}
+const Item = ({ item, width }) => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [count, setCount] = useState(1);
+  const [isHovered, setIsHovered] = useState(false);
+
+  //strapi format
+  const {
+    palette: { neutral },
+  } = useTheme();
+  const { category, price, name, image } = item.attributes;
+  const {
+    data: {
+      attributes: {
+        formats: {
+          medium: { url },
+        },
+      },
+    },
+  } = image;
+};
+
+export default Item;
