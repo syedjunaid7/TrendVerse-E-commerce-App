223fb31ea4c64e5cb86962e355b76671d6d3ecfa
syedjunaid7 <99808491+syedjunaid7@users.noreply.github.com>
2023-09-04 19:25:24 +0530

    c

--- src/scenes/home/Home.jsx
+++ src/scenes/home/Home.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import ShoppingList from "./ShoppingList";
 import MainCarousel from "./MainCarousel";
 export default function Home() {
-  return <div>
-  <MainCarousel />
-  </div>;
+  return (
+    <div>
+      <MainCarousel />
+      <ShoppingList />
+    </div>
+  );
 }
