356b95df0830f60a71cd224737a7a4531f0f00fa
syedjunaid7 <99808491+syedjunaid7@users.noreply.github.com>
2023-09-05 23:14:33 +0530

    c

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import ItemDetails from "./scenes/itemDetails/ItemDetails";
 import Checkout from "./scenes/checkout/Checkout";
 import Confirmation from "./scenes/checkout/Confirmation";
 import Navbar from "./scenes/global/Navbar";
+import CartMenu from "./scenes/global/CartMenu";
+import Footer from "./scenes/global/CartMenu"
 const ScrollToTop = () => {
   const { pathname } = useLocation();
   useEffect(() => {
@@ -24,6 +26,8 @@ function App() {
           <Route path="checkout" element={<Checkout />} />
           <Route path="checkout/success" element={<Confirmation />} />
         </Routes>
+        <CartMenu />
+        <Footer />
       </BrowserRouter>
     </div>
   );
